f3010feeb9013699ef7406b9b1f7ffaddafdc06b
bhaskar tester <Siva@Siva-PC>
2017-08-03 22:45:39 +0530

    chander

--- Testgit/src/Test/Testgitprac.java
+++ Testgit/src/Test/Testgitprac.java
@@ -5,7 +5,7 @@ public class Testgitprac {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(" test git ");
+		System.out.println(" test git 12 ");
 	}
 
 }
